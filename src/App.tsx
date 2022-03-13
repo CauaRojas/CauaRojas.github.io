0f8e97a15bd5c6f45d195be495cdee2982ffe28d
Cauã Rojas <cauarojas@hotmail.com>
2022-03-13 15:52:11 -0300

    adicionei o css basico

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,21 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-	return <h1>Hello World!</h1>;
+	return (
+		<>
+			<header>Cauã Alencar Rojas Romero</header>
+			<main>
+				<h1>Meus Projetos:</h1>
+				<ul>
+					<li>Teste</li>
+					<li>Teste</li>
+					<li>Teste</li>
+					<li>Teste</li>
+				</ul>
+			</main>
+			<footer>Contatos</footer>
+		</>
+	);
 }
 
 export default App;
